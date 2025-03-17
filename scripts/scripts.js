873194a7e68ca325a84da9d58b70c43422828de6
Jasmine Radi <jasmineradii@gmail.com>
2025-03-17 00:22:57 -0500

    final changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,17 @@
+function showSidebar() {
+	const sidebar = document.querySelector('.sidebar')
+	sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+	const sidebar = document.querySelector('.sidebar')
+	sidebar.style.display = 'none'
+}
+
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
